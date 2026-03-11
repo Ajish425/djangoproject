33071ee115ed8b7dbfe8ef86b72710ba0a529283
Ajish425 <ajishr365@outlook.com>
2026-03-11 23:53:56 +0530

    models added

--- zepto/models.py
+++ zepto/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Zepto(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    mrp = models.IntegerField()
+    quantity = models.IntegerField()
+    discount_percent = models.FloatField()
+    available_quantity = models.IntegerField()
+    discounted_selling_price= models.FloatField()
+    weight_in_gms = models.IntegerField()
+    out_of_stack = models.BooleanField()
+    quantity = models.IntegerField()
